4333eca8491bb39f5a0128bd441edfcd60620330
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 21:10:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,14 @@
 MakerCloud_KOI.registerTopicMessageHandler("BVKZV6T5", function (receivedMessage) {
     if (receivedMessage == "up") {
-        up()
+        powerbrick.Servo(powerbrick.Servos.S8, -45)
+        powerbrick.Servo(powerbrick.Servos.S1, 225)
     } else {
         if (receivedMessage == "down") {
-            down()
+            powerbrick.Servo(powerbrick.Servos.S1, -45)
+            powerbrick.Servo(powerbrick.Servos.S8, 225)
         }
     }
 })
-function up () {
-    powerbrick.Servo(powerbrick.Servos.S8, -45)
-    powerbrick.Servo(powerbrick.Servos.S1, 225)
-}
-function down () {
-    powerbrick.Servo(powerbrick.Servos.S1, -45)
-    powerbrick.Servo(powerbrick.Servos.S8, 225)
-}
 MakerCloud.setupWifi("waiwai", "51352343")
 MakerCloud_KOI.connectMakerCloudMQTT()
 MakerCloud_KOI.subscribeTopic("BVKZV6T5")
