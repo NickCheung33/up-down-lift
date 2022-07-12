3efe2e875b22636eec586bc7e051ac07e9bb2108
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 20:50:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,11 @@
 MakerCloud_KOI.registerTopicMessageHandler("BVKZV6T5", function (receivedMessage) {
     if (receivedMessage == "up") {
-        for (let index = 0; index < 4; index++) {
-            powerbrick.Servo(powerbrick.Servos.S8, -45)
-            powerbrick.Servo(powerbrick.Servos.S1, 225)
-        }
-    } else {
-        if (receivedMessage == "down") {
-            for (let index = 0; index < 4; index++) {
-                powerbrick.Servo(powerbrick.Servos.S1, -45)
-                powerbrick.Servo(powerbrick.Servos.S8, 225)
-            }
-        }
+        powerbrick.Servo(powerbrick.Servos.S8, -45)
+        powerbrick.Servo(powerbrick.Servos.S1, 225)
+    }
+    if (receivedMessage == "down") {
+        powerbrick.Servo(powerbrick.Servos.S1, -45)
+        powerbrick.Servo(powerbrick.Servos.S8, 225)
     }
 })
 koi.koi_init_pw(koi.SerialPorts.PORT2)
