47351ea606354032490c313bd6cc30d79fd9bbf5
NickCheung33 <107850266+NickCheung33@users.noreply.github.com>
2022-07-12 20:07:50 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
+MakerCloud_KOI.registerTopicMessageHandler("BVKZV6T5", function (receivedMessage) {
+    if (receivedMessage == "up") {
+        for (let index = 0; index < 4; index++) {
+            powerbrick.Servo(powerbrick.Servos.S8, 25)
+        }
+    } else {
+        if (receivedMessage == "down") {
+            for (let index = 0; index < 4; index++) {
+                powerbrick.Servo(powerbrick.Servos.S8, -25)
+            }
+        }
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        powerbrick.Servo(powerbrick.Servos.S8, 25)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        powerbrick.Servo(powerbrick.Servos.S8, -25)
+    }
+})
+koi.koi_init_pw(koi.SerialPorts.PORT2)
+koi.koi_lcd_direction(koi.LcdDirection.Front)
+MakerCloud.setupWifi("", "")
+MakerCloud_KOI.connectMakerCloudMQTT()
+MakerCloud_KOI.subscribeTopic("BVKZV6T5")
